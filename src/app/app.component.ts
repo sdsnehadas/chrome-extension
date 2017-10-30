c923aadae1e977eab089fb1618d09817a316bb71
SnehaD <snehad@qburst.com>
2017-10-30 10:17:41 +0530

    added background slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public imagesList ;
+
+  constructor() { }
+  ngOnInit() {
+    setInterval(this.changeBackground, 6000);
+  }
+
+  changeBackground() {
+    this.imagesList = [
+      { url: '../assets/background.jpg' },
+      { url: '../assets/background2.jpg' },
+      { url: '../assets/mybg1.jpg' },
+      { url: '../assets/background3.jpg' },
+      { url: '../assets/12.jpg' }];
+    document.body.style.backgroundImage = 'url(' +this.imagesList[Math.floor( Math.random() * 4)].url+ ')' ;
+  }
+
 }
